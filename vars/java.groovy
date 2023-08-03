4ea90749d3fc548a9fb0909a056893f8104f4a17
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-03 19:12:50 +0530

    Auto push

---
+++ vars/java.groovy
@@ -0,0 +1,35 @@
+def lintchecks()
+{   
+    sh '''
+             echo Performing lintchecks for ${COMPONENT}
+             mvn checkstyle:check || true
+             echo lint checks completed for ${COMPONENT}
+    '''
+
+}
+
+def call(COMPONENT) {
+
+    pipeline{
+         agent { label 'WS' }
+         stages{                                                // Start of the stages
+            stage('Lint Checks'){
+                  steps{
+                     script {
+                     lintchecks()
+               }    
+              
+            }
+        }
+             stage('Code Compile'){
+                   steps{
+                      sh "mvn clean compile"
+                }
+             }
+         }                                                      // End of the stages      
+    } 
+}
+
+//   script {
+//                         sample.info("CATALOGUE")
+//                     }
