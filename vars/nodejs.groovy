68e46dd94b432b768c494856c91eb24fd5945245
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-03 19:01:53 +0530

    Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,14 +1,15 @@
 def lintchecks()
 {   
     sh '''
-             echo Installing JSLint
+             echo Installing JSLint for ${COMPONENT}
              npm i jslint
              node_modules/jslint/bin/jslint.js server.js || true
+             echo lint checks completed for ${COMPONENT}
     '''
 
 }
 
-def call() {
+def call(COMPONENT) {
 
     pipeline{
          agent { label 'WS' }
