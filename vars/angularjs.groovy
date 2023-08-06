d7d42ca7c5241870e83f3084aa6f4c2fc25d2c06
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-06 18:40:01 +0530

    Auto push

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -1,7 +1,7 @@
 def lintchecks()
 {   
     sh '''
-             echo Installing AngularLint for ${COMPONENT}
+             echo Installing AnguularjsLint  for ${COMPONENT}
              echo lint checks completed for ${COMPONENT}
     '''
 
@@ -11,20 +11,43 @@ def call(COMPONENT) {
 
     pipeline{
          agent { label 'WS' }
+           environment {
+             SONARCRED =  credentials('SONARCRED')
+             SONAR_URL = "172.31.21.58"
+         }
          stages{                                                // Start of the stages
             stage('Lint Checks'){
                   steps{
                      script {
                      lintchecks()
-               }    
+                   }    
               
+                 }
             }
-        }
+              
              stage('Code Quality Analysis'){
                    steps{
-                      sh "echo Code Quality Analysis is in place"
+                        script{
+                            env.ARGS="-Dsonar.sources=."
+                            common.sonarchecks()
+                        }
                 }
              }
+             stage('Sonar Checks'){
+                  steps{
+                     script {
+                        env.ARGS="-Dsonar.java.binaries=target/"
+                        common.sonarchecks()
+                   }    
+              
+                }
+            }
+            stage('Testing'){
+                   steps{
+                            sh "echo Testing in Progress"
+                        }
+                }
+            }
          }                                                      // End of the stages      
     } 
-}
+
