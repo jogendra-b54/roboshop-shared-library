0db08ea8574c18544249ade74fd92649127800b8
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-06 17:12:49 +0530

    Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -14,16 +14,13 @@ def sonarchecks()
     sh '''
              echo Sonar Checks In Progress
              sonar-scanner -Dsonar.sources=.  -Dsonar.login=0137a6f92286deae048a73f8f632c18530ae98ca -Dsonar.host.url=http://54.90.171.239:9000 -Dsonar.projectKey=${COMPONENT}
+             curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh
+             sonar-quality-gate.sh admin password 172.31.21.58 ${COMPONENT}
              echo Sonar Checks are completed
     '''
 
 }
-
-def call(COMPONENT) {
-
-    pipeline{
-         agent { label 'WS' }
-         stages{                                                // Start of the stages
+          stages{                                                // Start of the stages
             stage('Lint Checks'){
                   steps{
                      script {
