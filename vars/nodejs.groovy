010390458b66f3ac36d365c23b42ffb844de1729
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-07 19:01:52 +0530

    Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -75,9 +75,11 @@ def call(COMPONENT){
             stage('Upload Artifacts'){
                 when { expression { env.TAG_NAME !=null } }
                 steps{
-                    sh "echo uploading ${COMPONENT} Artifacts to Nexus"
-                    sh "curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.95.24:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
-                    sh "echo Uploading ${COMPONENT} Artifacts to Nexus is Completed"
+                    sh '''
+                        echo uploading ${COMPONENT} Artifacts to Nexus
+                        curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.95.24:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+                        echo Uploading ${COMPONENT} Artifacts to Nexus is Completed
+                     '''
                 }
             }
         }
