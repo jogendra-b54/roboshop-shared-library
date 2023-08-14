90851505254a2f577d8b78d1956b06540d8e9cf8
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-14 19:47:34 +0530

    Auto push

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,51 +1,57 @@
-def lintchecks()
-{   
-    sh '''
-             echo Performing lintchecks for ${COMPONENT}
-             mvn checkstyle:check || true
-             echo lint checks completed for ${COMPONENT}
-    '''
-
+def call()
+{
+   node {
+      common.lintchecks()
+   }
 }
+// def lintchecks()
+// {   
+//     sh '''
+//              echo Performing lintchecks for ${COMPONENT}
+//              mvn checkstyle:check || true
+//              echo lint checks completed for ${COMPONENT}
+//     '''
 
-def call(COMPONENT) {
+// }
 
-    pipeline{
-         agent { label 'WS' }
-           environment {
-             SONARCRED =  credentials('SONARCRED')
-             SONAR_URL = "172.31.21.58"
-         }
-         stages{                                                // Start of the stages
-            stage('Lint Checks'){
-                  steps{
-                     script {
-                     lintchecks()
-                   }    
+// def call(COMPONENT) {
+
+//     pipeline{
+//          agent { label 'WS' }
+//            environment {
+//              SONARCRED =  credentials('SONARCRED')
+//              SONAR_URL = "172.31.21.58"
+//          }
+//          stages{                                                // Start of the stages
+//             stage('Lint Checks'){
+//                   steps{
+//                      script {
+//                      lintchecks()
+//                    }    
               
-                 }
-            }
+//                  }
+//             }
               
-             stage('Code Compile'){
-                   steps{
-                      sh "mvn clean compile"
-                }
-             }
-             stage('Sonar Checks'){
-                  steps{
-                     script {
-                        env.ARGS="-Dsonar.java.binaries=target/"
-                        common.sonarchecks()
-                   }    
+//              stage('Code Compile'){
+//                    steps{
+//                       sh "mvn clean compile"
+//                 }
+//              }
+//              stage('Sonar Checks'){
+//                   steps{
+//                      script {
+//                         env.ARGS="-Dsonar.java.binaries=target/"
+//                         common.sonarchecks()
+//                    }    
               
-                }
-            }
-            stage('Testing'){
-                   steps{
-                            sh "echo Testing in Progress"
-                        }
-                }
-            }
-         }                                                      // End of the stages      
-    } 
+//                 }
+//             }
+//             stage('Testing'){
+//                    steps{
+//                             sh "echo Testing in Progress"
+//                         }
+//                 }
+//             }
+//          }                                                      // End of the stages      
+//     } 
 
