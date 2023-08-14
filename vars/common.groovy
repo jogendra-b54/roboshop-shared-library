57d7fd45a0a593de17093e2cfd3b032c90e70fba
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-14 20:12:20 +0530

    Auto push

--- vars/common.groovy
+++ vars/common.groovy
@@ -48,14 +48,14 @@ def lintchecks()
     }
 
 }
-def sonarchecks()
-{   
+// def sonarchecks()
+// {   
     
-         sh  'echo Sonar Checks In Progress'
-         sh  'sonar-scanner -Dsonar.host.url=http://172.31.21.58:9000  ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin  -Dsonar.password=password'
-         sh  'curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh'
-             //sonar-quality-gate.sh ${SONARCRED_USR} ${SONARCRED_PSW} ${SONAR_URL} ${COMPONENT}
-         sh  'echo Sonar Checks are completed'
+//          sh  'echo Sonar Checks In Progress'
+//          sh  'sonar-scanner -Dsonar.host.url=http://172.31.21.58:9000  ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin  -Dsonar.password=password'
+//          sh  'curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh'
+//              //sonar-quality-gate.sh ${SONARCRED_USR} ${SONARCRED_PSW} ${SONAR_URL} ${COMPONENT}
+//          sh  'echo Sonar Checks are completed'
     
 
-}
+// }
--- vars/python.groovy
+++ vars/python.groovy
@@ -1,45 +1,52 @@
-def lintchecks()
-{   
-    sh '''
-             echo Installing PyLint for ${COMPONENT}
-             #pip3 install pylint
-             #pylint *.py
-             echo lint checks completed for ${COMPONENT}
-    '''
 
+def call()
+{
+   node {
+      common.lintchecks()   
+   }
 }
+// def lintchecks()
+// {   
+//     sh '''
+//              echo Installing PyLint for ${COMPONENT}
+//              #pip3 install pylint
+//              #pylint *.py
+//              echo lint checks completed for ${COMPONENT}
+//     '''
 
-def call(COMPONENT) {
+// }
 
-    pipeline{
-         agent { label 'WS' }
-           environment {
-             SONARCRED =  credentials('SONARCRED')
-             SONAR_URL = "172.31.21.58"
-         }
-         stages{                                                // Start of the stages
-            stage('Lint Checks'){
-                  steps{
-                     script {
-                     lintchecks()
-               }    
+// def call(COMPONENT) {
+
+//     pipeline{
+//          agent { label 'WS' }
+//            environment {
+//              SONARCRED =  credentials('SONARCRED')
+//              SONAR_URL = "172.31.21.58"
+//          }
+//          stages{                                                // Start of the stages
+//             stage('Lint Checks'){
+//                   steps{
+//                      script {
+//                      lintchecks()
+//                }    
               
-            }
-        }
-             stage('Sonar Checks'){
-                   steps{
-                        script{
-                            env.ARGS="-Dsonar.sources=."
-                            common.sonarchecks()
-                        }
-                }
-             }
-            stage('Testing'){
-                   steps{
-                          sh "echo Testing"
-                        }
-                }
-             }
-         }                                                      // End of the stages      
-    } 
+//             }
+//         }
+//              stage('Sonar Checks'){
+//                    steps{
+//                         script{
+//                             env.ARGS="-Dsonar.sources=."
+//                             common.sonarchecks()
+//                         }
+//                 }
+//              }
+//             stage('Testing'){
+//                    steps{
+//                           sh "echo Testing"
+//                         }
+//                 }
+//              }
+//          }                                                      // End of the stages      
+//     } 
 
