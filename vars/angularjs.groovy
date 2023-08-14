90119fde2e727e1f83c1306dcc5fcd069aea80da
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-14 20:21:12 +0530

    Auto push

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -1,53 +1,60 @@
-def lintchecks()
-{   
-    sh '''
-             echo Installing AnguularjsLint  for ${COMPONENT}
-             echo lint checks completed for ${COMPONENT}
-    '''
+def call()
+{
+   node {
+      common.lintchecks()
+   }
+} 
 
-}
+// def lintchecks()
+// {   
+//     sh '''
+//              echo Installing AnguularjsLint  for ${COMPONENT}
+//              echo lint checks completed for ${COMPONENT}
+//     '''
 
-def call(COMPONENT) {
+// }
 
-    pipeline{
-         agent { label 'WS' }
-           environment {
-             SONARCRED =  credentials('SONARCRED')
-             SONAR_URL = "172.31.21.58"
-         }
-         stages{                                                // Start of the stages
-            stage('Lint Checks'){
-                  steps{
-                     script {
-                     lintchecks()
-                   }    
+// def call(COMPONENT) {
+
+//     pipeline{
+//          agent { label 'WS' }
+//            environment {
+//              SONARCRED =  credentials('SONARCRED')
+//              SONAR_URL = "172.31.21.58"
+//          }
+//          stages{                                                // Start of the stages
+//             stage('Lint Checks'){
+//                   steps{
+//                      script {
+//                      lintchecks()
+//                    }    
               
-                 }
-            }
+//                  }
+//             }
               
-             stage('Code Quality Analysis'){
-                   steps{
-                        script{
-                            env.ARGS="-Dsonar.sources=."
-                            common.sonarchecks()
-                        }
-                }
-             }
-             stage('Sonar Checks'){
-                  steps{
-                     script {
-                        env.ARGS="-Dsonar.java.binaries=target/"
-                        common.sonarchecks()
-                   }    
+//              stage('Code Quality Analysis'){
+//                    steps{
+//                         script{
+//                             env.ARGS="-Dsonar.sources=."
+//                             common.sonarchecks()
+//                         }
+//                 }
+//              }
+//              stage('Sonar Checks'){
+//                   steps{
+//                      script {
+//                         env.ARGS="-Dsonar.java.binaries=target/"
+//                         common.sonarchecks()
+//                    }    
               
-                }
-            }
-            stage('Testing'){
-                   steps{
-                            sh "echo Testing in Progress"
-                        }
-                }
-            }
-         }                                                      // End of the stages      
-    } 
+//                 }
+//             }
+//             stage('Testing'){
+//                    steps{
+//                             sh "echo Testing in Progress"
+//                         }
+//                 }
+//             }
+//          }                                                      // End of the stages      
+//     } 
 
