eb7f9af5f9fc969c374b965a223bb708a26f41c2
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-16 12:40:46 +0530

    Auto push

--- vars/common.groovy
+++ vars/common.groovy
@@ -119,9 +119,9 @@ def artifacts(){
             stage('Upload Artifacts'){
                  if(env.APP_TYPE == "nodejs"){
                     sh '''
-                         echo preparing the Artifacts for ${COMPONENT}
-                         npm install
-                         zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
+                            echo uploading ${COMPONENT} Artifacts to Nexus
+                            curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.95.24:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+                            echo Uploading ${COMPONENT} Artifacts to Nexus is Completed
                     '''
                 }
             }
