29c84fd93dfe0f6bd3a6a923a7aa47b95ff68f72
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-19 17:31:07 +0530

    Auto push

--- vars/common.groovy
+++ vars/common.groovy
@@ -91,7 +91,7 @@ def artifacts(){
              print UPLOAD_STATUS
         }
                     
-        if(env.UPLOAD_STATUS != "null"){
+        if(env.UPLOAD_STATUS == ""){
             stage('Prepare Artifacts'){
                     if(env.APP_TYPE == "nodejs"){
                     sh '''
