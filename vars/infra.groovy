d292e91564fbd960ceb395863801bd4a48ad6b82
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-02 22:45:02 +0530

    Auto push

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -10,17 +10,17 @@ def call() {
           git branch: 'main', url: "https://github.com/jogendra-b54/${REPONAME}.git"
 
           stage('terraform init') {
-                sh "cd ${REPONAME}"
+                //sh "cd ${REPONAME}"
                  sh "terrafile -f env-${ENV}/Terrafile"
                  sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"
              }
         stage('terraform plan') {
-                 sh "cd ${REPONAME}"
+                 //sh "cd ${REPONAME}"
                  sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
 
         }
         stage('terraform Action') {
-                 sh "cd ${REPONAME}"
+                 //sh "cd ${REPONAME}"
                  sh "terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars"
 
              }
